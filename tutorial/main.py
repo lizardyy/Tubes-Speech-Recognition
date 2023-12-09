d29fea53bc564e1b9675094ac27496da622ea68c
Mahesa Lizardy <88304455+lizardyy@users.noreply.github.com>
2023-12-09 20:39:13 +0700

    add model and main.py live, main.ipynb audio file

---
+++ tutorial/main.py
@@ -0,0 +1,11 @@
+from pocketsphinx import LiveSpeech
+
+
+hmm = 'dataset/model_parameters/dataset.cd_cont_200'
+lm ='dataset/etc/dataset.lm.bin'
+dict = 'dataset/etc/dataset.dict'
+
+recognizer = LiveSpeech(verbose=False, sampling_rate=44100, buffer_size=2048, no_search=False, full_utt=False, hmm=hmm,lm=lm, dict=dict)
+
+for phrase in recognizer:
+    print(phrase)
