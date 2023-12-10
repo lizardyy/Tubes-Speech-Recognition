9419d60526678240551ed33c7b797866d0260a05
Mahesa Lizardy <88304455+lizardyy@users.noreply.github.com>
2023-12-10 16:28:19 +0700

    push model sr 16000

--- tutorial/main.py
+++ tutorial/main.py
@@ -1,11 +1,11 @@
 from pocketsphinx import LiveSpeech
 
 
-hmm = 'dataset/model_parameters/dataset.cd_cont_200'
+hmm = '../Model/Model_SR_16000/model_parameters/dataset.cd_cont_200'
 lm ='dataset/etc/dataset.lm.bin'
 dict = 'dataset/etc/dataset.dict'
 
-recognizer = LiveSpeech(verbose=False, sampling_rate=44100, buffer_size=2048, no_search=False, full_utt=False, hmm=hmm,lm=lm, dict=dict)
+recognizer = LiveSpeech(verbose=False, sampling_rate=16000, buffer_size=2048, no_search=False, full_utt=False, hmm=hmm,lm=lm, dict=dict)
 
 for phrase in recognizer:
     print(phrase)
